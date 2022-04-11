58c9c6d02ac00c17e9e9cf4d12dc581b9c013145
Bandruf <75857420+Bandruf@users.noreply.github.com>
2022-04-10 21:36:22 -0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+let Bitches_txt = document.getElementById("bitches")
+
+document.getElementById("Calculate").onclick = function(){
+    let bs = Math.floor(Math.random(-1,5) * 10)
+    let name = document.getElementById("name-s").value.toLowerCase()
+    let removed = name.split(" ").join("");
+    console.log(removed)
+    Bitches_txt.innerHTML = "Calculating Bitches..."
+    setTimeout(() => { 
+        if (removed == "tanner" || removed == "tannergist") {
+            Bitches_txt.innerHTML = "You Get 1000+ Guys!"
+        } else {
+            if (bs == 1) {
+                Bitches_txt.innerHTML = "You Get "+bs+" Bitch!"  
+            } else {
+                Bitches_txt.innerHTML = "You Get "+bs+" Bitches!"
+            }
+        }
+    }, 1000);
+}
+
+document.getElementById("main-div").style.display = "none"
+
+let pswrd = prompt("Password?")
+
+let pass = "bandrufthagreat"
+
+if (pswrd == pass) {
+    document.getElementById("main-div").style.display = "flex"
+    alert("welcome")
+} else {
+    alert("wrong pass")
+}
